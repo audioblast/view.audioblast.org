dd00434feb847a9eb186b5e1bdf3090e3818d262
Ed Baker <edwbaker@gmail.com>
2020-11-27 16:43:44 +0000

    Rearrange <head>.

--- index.php
+++ index.php
@@ -3,14 +3,13 @@
 <title>audioBLAST! Viewer</title>
 <link rel="stylesheet" href="https://view.audioblast.org/ab-view.css">
 <link rel="stylesheet" href="https://cdn.audioblast.org/autoComplete.js/dist/css/autoComplete.css">
-<script type="text/javascript" src="https://view.audioblast.org/core/js/core.js"></script>
-<script src="https://cdn.audioblast.org/autoComplete.js/dist/js/autoComplete.min.js"></script>
-<script src="https://cdn.audioblast.org/plotly.js/dist/plotly.min.js"></script>
 <link rel="stylesheet" href="https://cdn.audioblast.org/ab-api.css">
 <link href="https://cdn.audioblast.org/tabulator/dist/css/tabulator.min.css" rel="stylesheet">
-<script type="text/javascript" src="https://cdn.audioblast.org/tabulator/dist/js/tabulator.min.js"></script>
-<script type="text/javascript" src="https://cdn.audioblast.org/ab-tabulator.js"></script>
-<link rel="stylesheet" href="//code.jquery.com/ui/1.10.3/themes/smoothness/jquery-ui.css">
+<script type="text/javascript" src="https://view.audioblast.org/core/js/core.js"></script>
+<script src="https://cdn.audioblast.org/autoComplete.js/dist/js/autoComplete.min.js" defer></script>
+<script src="https://cdn.audioblast.org/plotly.js/dist/plotly.min.js" defer></script>
+<script type="text/javascript" src="https://cdn.audioblast.org/tabulator/dist/js/tabulator.min.js" defer></script>
+<script type="text/javascript" src="https://cdn.audioblast.org/ab-tabulator.js" defer></script>
 </head>
 
 <body>
