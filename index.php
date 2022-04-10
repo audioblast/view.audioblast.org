69b7f5ce4a6ab0cf273dc4a918534bd071dfbf3a
Ed Baker <edwbaker@gmail.com>
2022-04-10 15:12:48 +0100

    Clean formatting of html

--- index.php
+++ index.php
@@ -3,9 +3,9 @@
 <title>audioBLAST! Viewer</title>
 <link rel="stylesheet" href="https://view.audioblast.org/ab-view.css">
 <link rel="stylesheet" href="https://cdn.audioblast.org/ab-api.css">
-<link href="https://cdn.audioblast.org/tabulator/dist/css/tabulator.min.css" rel="stylesheet">
+<link rel="stylesheet" href="https://cdn.audioblast.org/tabulator/dist/css/tabulator.min.css">
 <script type="text/javascript" src="https://view.audioblast.org/core/js/core.js"></script>
-<script src="https://cdn.audioblast.org/plotly.js/dist/plotly.min.js" defer></script>
+<script type="text/javascript" src="https://cdn.audioblast.org/plotly.js/dist/plotly.min.js" defer></script>
 <script type="text/javascript" src="https://cdn.audioblast.org/tabulator/dist/js/tabulator.min.js" defer></script>
 <script type="text/javascript" src="https://cdn.audioblast.org/ab-tabulator.js" defer></script>
 <?php include("core/includes/head.php"); ?>
