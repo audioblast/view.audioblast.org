1ceda9cb53bbbe01bddcee3d4d37188b7bb71b71
Ed Baker <edwbaker@gmail.com>
2022-04-10 16:53:18 +0100

    Move to onload.

--- core/includes/component.js
+++ core/includes/component.js
@@ -1,8 +1,10 @@
-var availableComponents = ["bedoyaAB", "tdscAB", "biAB", "audiowaveformAB", "aciAB"];
-var availableComponentsNames = ["Rainfall (Bedoya method)", "TDSC", "Bioacoustic Index", "Waveform", "Acoustic Complexity"];
-var componentList = document.getElementById("components");
-for (i=0; i < availableComponents.length; i++) {
-  var newComponent = document.createElement('li');
-  newComponent.innerHTML = "<a onclick='viewAB.addPlugin("+availableComponents[i]+")'>"+availableComponentsNames[i]+"</a>";
-  componentList.appendChild(newComponent);
+document.onload = function() {
+  var availableComponents = ["bedoyaAB", "tdscAB", "biAB", "audiowaveformAB", "aciAB"];
+  var availableComponentsNames = ["Rainfall (Bedoya method)", "TDSC", "Bioacoustic Index", "Waveform", "Acoustic Complexity"];
+  var componentList = document.getElementById("components");
+  for (i=0; i < availableComponents.length; i++) {
+    var newComponent = document.createElement('li');
+    newComponent.innerHTML = "<a onclick='viewAB.addPlugin("+availableComponents[i]+")'>"+availableComponentsNames[i]+"</a>";
+    componentList.appendChild(newComponent);
+  }
 }
