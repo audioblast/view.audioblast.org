40a5a9cfe827d665a1a708c19aec899a7bb7fac6
Ed Baker <edwbaker@gmail.com>
2022-04-10 15:09:50 +0100

    Update head.php

---
+++ core/includes/head.php
@@ -0,0 +1,6 @@
+<script type="text/javascript" src="https://view.audioblast.org/core/includes/component.js"></script>
+<script type="text/javascript" src="https://view.audioblast.org/components/bedoya/component.js"></script>
+<script type="text/javascript" src="https://view.audioblast.org/components/tdsc/component.js"></script>
+<script type="text/javascript" src="https://view.audioblast.org/components/bi/component.js"></script>
+<script type="text/javascript" src="https://view.audioblast.org/components/audiowaveform/component.js"></script>
+<script type="text/javascript" src="https://view.audioblast.org/components/aci/component.js"></script>
