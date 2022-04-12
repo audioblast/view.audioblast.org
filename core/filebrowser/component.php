066c4c393284a1ca54a1110ace54cbb5c2eda591
Ed Baker <edwbaker@gmail.com>
2022-04-12 11:22:04 +0100

    Use zoom images.

--- core/filebrowser/component.php
+++ core/filebrowser/component.php
@@ -10,12 +10,16 @@
 
 <div id="zoom-control">
 <ul class="ulhoriz">
-<li class="big">
-  <a onclick="window.open('https://audioblast.org/audioblast.php?source=<?php print($_GET['source']); ?>&id=<?php print($_GET['id']); ?>', '_self');">alphaBLAST!</a>
+<li>
+  <a onclick="window.open('https://audioblast.org/audioblast.php?source=<?php print($_GET['source']); ?>&id=<?php print($_GET['id']); ?>', '_self');">
+    <img src="https://cdn.audioblast.org/audioblast_flash.png" /></a>
 </li>
-<li><a onclick="viewAB.zoomOut()">-</a></li>
-<li><a onclick="viewAB.zoomIn()">+</a></li>
-<li><a onclick="viewAB.zoomFit()">[-]</a></li>
+<li><a onclick="viewAB.zoomOut()">
+  <img src="https://cdn.audioblast.org/audioblast_zoom_out.png" /></a></li>
+<li><a onclick="viewAB.zoomIn()">
+  <img src="https://cdn.audioblast.org/audioblast_zoom_in.png" /></a></li>
+<li><a onclick="viewAB.zoomFit()">
+  <img src="https://cdn.audioblast.org/audioblast_zoom_fit.png" /></a></li>
 </ul>
 </div>
 
