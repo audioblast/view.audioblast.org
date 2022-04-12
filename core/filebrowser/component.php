931a9d2e366164d9b2fa4bd36f1a694cea8ab3c2
Ed Baker <edwbaker@gmail.com>
2022-04-12 11:24:27 +0100

    Zoom button titles/tooltips.

--- core/filebrowser/component.php
+++ core/filebrowser/component.php
@@ -12,14 +12,14 @@
 <ul class="ulhoriz">
 <li>
   <a onclick="window.open('https://audioblast.org/audioblast.php?source=<?php print($_GET['source']); ?>&id=<?php print($_GET['id']); ?>', '_self');">
-    <img src="https://cdn.audioblast.org/audioblast_flash.png" /></a>
+    <img src="https://cdn.audioblast.org/audioblast_flash.png" title="alphaBLAST! Search"/></a>
 </li>
 <li><a onclick="viewAB.zoomOut()">
-  <img src="https://cdn.audioblast.org/audioblast_zoom_out.png" /></a></li>
+  <img src="https://cdn.audioblast.org/audioblast_zoom_out.png" title="Zoom Out"/></a></li>
 <li><a onclick="viewAB.zoomIn()">
-  <img src="https://cdn.audioblast.org/audioblast_zoom_in.png" /></a></li>
+  <img src="https://cdn.audioblast.org/audioblast_zoom_in.png" title="Zoom In"/></a></li>
 <li><a onclick="viewAB.zoomFit()">
-  <img src="https://cdn.audioblast.org/audioblast_zoom_fit.png" /></a></li>
+  <img src="https://cdn.audioblast.org/audioblast_zoom_fit.png" title= "Zoom to Fit"/></a></li>
 </ul>
 </div>
 
